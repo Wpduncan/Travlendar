0f44f2a89ea3be2e7b237c3eb3267dae42f56d2f
Miguel <32783007+Starr501@users.noreply.github.com>
2019-03-31 07:51:54 -0500

    Added to MapsActivity and enabled APIs
    Added to MapsActivity, Changed API key, Enabled Google Maps, Google Places, and Distanst Matrix.
    When returning from MapsActivity, will fill in some of the fields for events.

--- app/src/main/java/com/example/thetravlendar/MapsActivity.java
+++ app/src/main/java/com/example/thetravlendar/MapsActivity.java
@@ -1,17 +1,39 @@
 package com.example.thetravlendar;
 
-import android.support.v4.app.FragmentActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
+
+import com.google.android.gms.common.api.Status;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.api.net.PlacesClient;
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
+
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
-
+    String TAG = "placeautocomplete";
+    TextView txtView;
+    EditText eventView;
     private GoogleMap mMap;
 
     @Override
@@ -22,6 +44,57 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        txtView = findViewById(R.id.txtView);
+        eventView = findViewById(R.id.event_location);
+
+        // Initialize Places.
+        Places.initialize(getApplicationContext(), "AIzaSyDAhzX0Vvqd5Xnv7eyUHr5drHWdQwZgeq8");
+        // Create a new Places client instance.
+        PlacesClient placesClient = Places.createClient(this);
+
+        // Initialize the AutocompleteSupportFragment.
+        AutocompleteSupportFragment autocompleteFragment = (AutocompleteSupportFragment)
+                getSupportFragmentManager().findFragmentById(R.id.autocomplete_fragment);
+
+        // Specify the types of place data to return.
+        autocompleteFragment.setPlaceFields(Arrays.asList(Place.Field.ID, Place.Field.NAME,
+                 Place.Field.ADDRESS, Place.Field.LAT_LNG));
+
+        // Set up a PlaceSelectionListener to handle the response.
+        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(Place place) {
+                // TODO: Get info about the selected place.
+                //For Testing
+                txtView.setText(place.getName()+","+place.getAddress());
+                //
+                Log.i(TAG, "Place: " + place.getName() + ", " + place.getAddress());
+
+
+
+                StringTokenizer  address = new StringTokenizer(place.getAddress(),",");
+                String streetNum = address.nextToken();  // Street name and number
+                String city = address.nextToken(); // City name
+                StringTokenizer stateZip = new StringTokenizer(address.nextToken(), " ");
+                String state = stateZip.nextToken(); // State Name
+                String zip = stateZip.nextToken(); // Zip code
+                Intent i = new Intent(MapsActivity.this, AddEventActivity.class);
+                i.putExtra("street",streetNum);
+                i.putExtra("city", city);
+                i.putExtra("state", state);
+                i.putExtra("zip", zip);
+                i.putExtra("name", place.getName());
+                startActivity(i);
+            }
+
+            @Override
+            public void onError(Status status) {
+                // TODO: Handle the error.
+                Log.i(TAG, "An error occurred: " + status);
+            }
+        });
+
     }
 
 
@@ -42,5 +115,34 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
     }
+
+    /** @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.map_options, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Change the map type based on the user's selection.
+        switch (item.getItemId()) {
+            case R.id.normal_map:
+                mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+                return true;
+            case R.id.hybrid_map:
+                mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+                return true;
+            case R.id.satellite_map:
+                mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+                return true;
+            case R.id.terrain_map:
+                mMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }*/
+
 }
